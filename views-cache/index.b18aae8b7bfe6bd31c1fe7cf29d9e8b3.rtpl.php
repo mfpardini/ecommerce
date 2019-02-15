0da408c8a6ed479c88fe48cb27de0a8171961d73
mfpardini <matheusfpardini@gmail.com>
2019-02-15 12:14:24 -0200

    add tpl admin

---
+++ views-cache/index.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
@@ -0,0 +1,10 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?>
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Your Page Content Here -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
