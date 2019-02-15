77188a2794d0c5a4e031972d5843a07f0ed8daee
mfpardini <matheusfpardini@gmail.com>
2019-02-15 11:11:18 -0200

    class page altered

---
+++ views-cache/index.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,9 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>Teste da página funcionando tudo ok!</h1>
+<div>
+	<div style="background-color: green">
+		<button type="submmit">enviar</button>
+	</div>
+	<div style="background-color: blue">
+		teste
+	</div>
+</div>
